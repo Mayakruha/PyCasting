61235a974189acaeee3c9625f35d40764ea69251
Mayakruha <49606586+Mayakruha@users.noreply.github.com>
2026-03-23 14:44:49 +0100

    Update Common.py

--- PyCasting/Common.py
+++ PyCasting/Common.py
@@ -259,8 +259,8 @@ class Rect_tube(CCM):
 #--------------------------------------------------------------------
 #-CREEP FUNCTIONS: Stress[MPa]=func(EpsR[1/sec], Temp[C]))-----------
 #--------------------------------------------------------------------
-def creep_NISK(EpsR,Temp, A=21900E+6, k=0.2, lmbda=0.004):
-    return A*(EpsR**k)*exp(-(Temp+273)*lmbda)
+def creep_NISK(EpsR,Temp, A=21900E+6, mu=0.2, lmbda=0.004):
+    return A*(EpsR**mu)*exp(-(Temp+273)*lmbda)
 #---------------------------------
 #-------OUTPUT FUNCTIONS----------
 #---------------------------------
